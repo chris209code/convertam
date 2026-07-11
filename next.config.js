89a4206c978e555aba1b1dc3e8ed4145fc07cf06
chris209code <okekechris29@gmail.com>
2026-07-11 20:07:15 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      https: false,
+      http: false,
+      path: false,
+      stream: false,
+    };
+    return config;
+  },
+};
 
 module.exports = nextConfig;
